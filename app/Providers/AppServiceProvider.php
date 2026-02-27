4fbc08fa24769f51a317b23f8478a4b0eeb666db
Developer <dev@sisreservas.com>
2026-02-27 09:47:17 -0500

    fix: force https and trust all proxies for Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
