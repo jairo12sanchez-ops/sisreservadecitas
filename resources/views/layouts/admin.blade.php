19705ad74ff8412a43281ed2e5c9890b87bf68ba
Developer <dev@sisreservas.com>
2026-03-06 09:46:54 -0500

    UI: robust image pathing and cache busting for localhost and production

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -77,7 +77,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="" class="brand-link">
-            <img src="{{ asset('assets/img/logo_jasadent.jpeg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="{{ url('assets/img/logo_jasadent.jpeg') . '?v=' . time() }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">JasaDent</span>
         </a>
         <!-- Sidebar -->
